a491a115234a02f0da4d72b0dfb2ee6c24f55d1d
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-14 21:10:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(name='dblpy',
+      author='Niels Steenman',
+      author_email='ngssteenman@gmail.com',
+      url='https://github.com/iDutchy/gmailpy',
+      version='1.0',
+      packages = ['gmailpy'],
+      license='MIT',
+      description='A simple API wrapper for sending emails with Gmail',
+      long_description=readme,
+      keywords='gmail email',
+      download_url = 'https://github.com/iDutchy/gmailpy/archive/v1.0.tar.gz',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Internet',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Utilities',
+      ]
+      )
