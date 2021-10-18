72a7c2d24be7f3ac282e2481a69323fc32058386
iDutchy <42503862+iDutchy@users.noreply.github.com>
2021-10-18 02:50:17 +0200

    cleaning up
    cleaned up some code

--- gmailpy/client.py
+++ gmailpy/client.py
@@ -17,25 +17,25 @@ class Client:
         self.password = password
         
         
-    def send(self, receiver, body, subject=None, bcc=None, attachment_bytes=None, attachment_name=None):
+    def send(self, receiver, body, subject="No subject", bcc=None, attachment_bytes=None, attachment_name=None):
         sender_email = self.mail
         password = self.password
 
         message = MIMEMultipart()
         message["From"] = sender_email
         message["To"] = receiver
-        message["Subject"] = subject if subject else "No subject"
+        message["Subject"] = subject
         message["Bcc"] = bcc
 
         message.attach(MIMEText(body, "plain"))
         
-        if attachment_name is None and attachment_bytes is not None:
+        if not attachment_name and attachment_bytes:
             raise Error('You did not provide an attachment name for your attachment!')
             
-        if attachment_name is not None and attachment_bytes is None:
+        if attachment_name and not attachment_bytes:
             raise Error('Can not set an attachment name without an attachment!')
         
-        if attachment_bytes is not None and attachment_name is not None:
+        if attachment_bytes and attachment_name:
             part = MIMEBase("application", "octet-stream")
             part.set_payload(attachment_bytes)
 
@@ -55,7 +55,7 @@ class Client:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver, text)
 
-        async def send_async(self, receiver, body, subject=None, bcc=None, attachment_bytes=None, attachment_name=None):
+        async def send_async(self, receiver, body, subject="No subject", bcc=None, attachment_bytes=None, attachment_name=None):
             loop = asyncio.get_event_loop()
             sync_send = functools.partial(send, receiver, body, subject, bcc, attachment_bytes, attachment_name)
             return await loop.run_in_executor(None, sync_send)
